b732690343855fb1961b4909a85e3bd9df65d1b2
vedantpatil1600 <ptlvedant07@gmail.com>
2026-05-02 16:58:05 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,24 @@
+import { supabase } from './supabase.js'
+
+async function getUsers() {
+  const { data, error } = await supabase
+    .from('users')
+    .select('*')
+
+  if (error) {
+    console.error(error)
+    return
+  }
+
+  displayUsers(data)
+}
+
+function displayUsers(data) {
+  const container = document.getElementById("data")
+
+  data.forEach(user => {
+    container.innerHTML += `<p>${user.name} - ${user.email}</p>`
+  })
+}
+
+getUsers()
